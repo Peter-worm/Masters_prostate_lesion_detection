3369be50a7dc74c29a38fdaddb68992d22de8c8a
Peter-worm <1234ozmo@wp.pl>
2026-03-24 10:38:56 +0100

    trying to manage pipeline and make it look good

---
+++ My_preprocessing/settings/main_settings.py
@@ -0,0 +1,42 @@
+from core.helper import transform_step_list_to_dictioanry
+
+class test_settings:
+    def __init__(self):
+        self.original_data_folder =  '/home/robakp/Exeriments1/prostate_lesion_detection/rjozwiak-MGR_dataset_correct/MGR_dataset_correct'
+
+        self.channels = {
+            'adc' : 'adc',
+            'anatomy' : 'anatomy',
+            'dwi' : 'dwi',
+            't2' : 't2'
+        }
+
+        self.target = 'lesion'
+
+        self.file_extention = '.nii.gz'
+
+        self.preprocessing_steps_list = [
+            ('start', 'nifty'),
+            ('resampling','resampled'),
+            ('nifti_to_raw', 'raw'),
+            ('filling_anatomy_gaps', 'anatomy_gap_filled'),
+            ('cropping', 'cropped'),
+
+        ]
+
+        self.preprocessed_steps = transform_step_list_to_dictioanry(self.preprocessing_steps_list)
+
+        self.crop_size = (160,160,24)
+        self.target_spacing=(0.8, 0.8, 3.5)
+
+    def get_setting_dictionary(self):
+        return {
+            'original_data_folder': self.original_data_folder,
+            'channels': self.channels,
+            'target': self.target,
+            'file_extention': self.file_extention,
+            'preprocessing_steps_list': self.preprocessing_steps_list,
+            'preprocessed_steps': self.preprocessed_steps,
+            'crop_size': self.crop_size,
+            'target_spacing': self.target_spacing
+        }
